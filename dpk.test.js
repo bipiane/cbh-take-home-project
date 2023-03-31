98a2771f442a2d586b031b93165acb316817e3f3
Ivan Pianetti <ivanpianetti23@gmail.com>
2023-03-31 13:37:20 -0300

    code style

--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,8 @@
-const { deterministicPartitionKey } = require("./dpk");
+const {deterministicPartitionKey} = require("./dpk");
 
 describe("deterministicPartitionKey", () => {
-  it("Returns the literal '0' when given no input", () => {
-    const trivialKey = deterministicPartitionKey();
-    expect(trivialKey).toBe("0");
-  });
+    it("Returns the literal '0' when given no input", () => {
+        const trivialKey = deterministicPartitionKey();
+        expect(trivialKey).toBe("0");
+    });
 });
